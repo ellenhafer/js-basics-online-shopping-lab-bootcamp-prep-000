5f5f7e0e7fe6e97ea885a6568590f1abfdecb746
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-28 09:32:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,73 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ //create object containing two key-value pairs
+ var obj = {  
+    itemName: item,  
+    // generate random integer between 1 and 100
+    itemPrice: Math.floor(Math.random() * (100 - 1) + 1) 
+  };
+  //add object to cart array
+  cart.push(obj);
+  //return 'item' has been added to your cart
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  //if cart is empty, return following
+  if(cart.length === 0){
+    return "Your shopping cart is empty.";
+  }
+  //if cart length is 1, return following
+  else if(cart.length === 1){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  }
+  //if cart length is 2, return following
+  else if(cart.length === 2){
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  }
+  //otherwise, perform following
+  else {
+    //create blank array to store copy of cart
+    var newCart = [];
+    //get last item in cart and store it to variable lastItem
+    var lastItem = cart.slice(-1)[0]; 
+    //iterate through every item in cart except for last item
+    for(let i = 0; i < cart.length-1; i++){
+      //add following to newCart
+      newCart += `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+    }
+    //return following
+    return `In your cart, you have ${newCart}and ${lastItem.itemName} at $${lastItem.itemPrice}.`
+  }
 }
 
 function total() {
-  // write your code here
+  //create total variable to store running total
+  var total = 0;
+  //iterate through cart array
+  for(let i = 0; i < cart.length; i++) {
+    //add itemPrice of current item to total
+    total += cart[i].itemPrice;
+  }
+  //once loop has finished, return total value
+  return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  // iterate through cart array
+  for(let i = 0; i < cart.length; i++) {
+    // if itemName === item
+    if(item == cart[i].itemName){
+      console.log("Item is in cart");
+      // remove item from cart array
+      // return updated cart
+    }
+    // else return 'that item is not in your cart'
+    else {
+      return "That item is not in your cart."
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
